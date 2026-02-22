87896b6f9c52b84832df51fccfe22c3f038255a2
Ariadni <ariadni.machado@cdn.tv.br>
2026-02-22 04:02:28 -0300

    app page and app route create for named routes

---
+++ lib/core/routes/app_pages.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+import 'package:talent_crm_app/core/routes/app_routes.dart';
+import 'package:talent_crm_app/features/account/account_binding.dart';
+import 'package:talent_crm_app/features/account/presentation/account_shell.dart';
+import 'package:talent_crm_app/features/home/home_binding.dart';
+import 'package:talent_crm_app/features/home/presentation/home_shell.dart';
+
+class AppPages {
+  static const initial = AppRoutes.home;
+
+  static final pages = <GetPage>[
+    GetPage(
+      name: AppRoutes.home,
+      page: () => const HomeShell(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: AppRoutes.account,
+      page: () => const AccountShell(),
+      binding: AccountBinding(),
+    ),
+  ];
+}
