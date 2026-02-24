550014afe20cde79fb4b388e9007a2bd6476c022
Ariadni <ariadni.machado@cdn.tv.br>
2026-02-24 01:39:58 -0300

    separation to separate file

--- lib/features/home/presentation/widgets/employee/employee_card.dart
+++ lib/features/home/presentation/widgets/employee/employee_card.dart
@@ -3,54 +3,10 @@ import 'package:talent_crm_app/core/design/app_spacing.dart';
 import 'package:talent_crm_app/features/talent/entities/talent.dart';
 import 'package:talent_crm_app/l10n/app_localizations.dart';
 
-class HomeSectionEmployees extends StatelessWidget {
-  final String title;
-  final List<Talent> employees;
-
-  const HomeSectionEmployees({
-    super.key,
-    required this.title,
-    required this.employees,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    if (employees.isEmpty) {
-      return const SizedBox.shrink();
-    }
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: AppSpacing.lg),
-          child: Text(
-            title,
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-        ),
-        const SizedBox(height: AppSpacing.md),
-        ListView.separated(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          padding: const EdgeInsets.symmetric(horizontal: AppSpacing.lg),
-          itemCount: employees.length,
-          separatorBuilder: (_, __) => const SizedBox(height: AppSpacing.md),
-          itemBuilder: (context, index) {
-            final employee = employees[index];
-
-            return _EmployeeCard(employee: employee);
-          },
-        ),
-      ],
-    );
-  }
-}
-
-class _EmployeeCard extends StatelessWidget {
+class EmployeeCard extends StatelessWidget {
   final Talent employee;
 
-  const _EmployeeCard({required this.employee});
+  const EmployeeCard({super.key, required this.employee});
 
   @override
   Widget build(BuildContext context) {
--- test/presentation/home/widgets/employee/home_section_employees_test.dart
+++ test/presentation/home/widgets/employee/home_section_employees_test.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:network_image_mock/network_image_mock.dart';
+import 'package:talent_crm_app/features/home/presentation/widgets/employee/home_section_employees.dart';
 import 'package:talent_crm_app/features/talent/entities/contact_talent.dart';
 import 'package:talent_crm_app/features/talent/entities/talent.dart';
-import 'package:talent_crm_app/features/home/presentation/widgets/employee/employee_card.dart';
 
 import '../../../helpers/wrapper.dart';
 
